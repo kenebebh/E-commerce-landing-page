50d328b97d6b778aa9dcebf20dfd3bba7e4947b8
Kenebebh <kenebebhbanigo@gmail.com>
2022-04-10 22:53:12 +0100

    final changes

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const navbarContainer = document.querySelector(".nav__container");
 toggleButton.addEventListener("click", () => {
   navbarContainer.classList.toggle("active");
 });
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
+});
